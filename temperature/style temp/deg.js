b001015f755007ee4c733a0cd33b029d508dd5ee
E-s2ra <esra99san@gmail.com>
2026-02-23 22:58:11 +0300

    Add files via upload

---
+++ temperature/style temp/deg.js
@@ -0,0 +1,29 @@
+const textbox = document.getElementById("textbox");
+const tofarhenheit = document.getElementById("tofarhenheit");
+const tocelsiu = document.getElementById("tocelsiu");
+const result = document.getElementById("result");
+let temp;
+
+
+function convert() {
+
+    if (tofarhenheit.checked) {
+        temp = Number(textbox.value);
+        temp = temp * 9 / 5 + 32;
+        result.textContent = temp.toFixed(1) + "F"
+
+    }
+
+
+    else if (tocelsiu.checked) {
+        temp = Number(textbox.value);
+        temp = (temp - 32) * (5 / 9);
+        result.textContent = temp.toFixed(1) + "C"
+
+    } else {
+        result.textContent = "select a unit"
+    }
+
+
+}
+
